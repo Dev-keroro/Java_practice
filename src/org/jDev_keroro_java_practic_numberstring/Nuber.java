6c210a4fae0da47235e65e5cd1477f8cfd391b3a
angelred551 <angelred551@gmail.com>
2019-07-18 18:48:51 +0900

    숫자와 문자3
    1. 문자열 더하기 문자열 가능하다. ("문자열" + "문자열")
    2. 문자열에 큰따옴표를 (" ") 표시하고 싶다면 역슬래쉬 \ 를 이용하여 표시할수 있다 ( \" 큰따옴표 표시 \" )

--- src/org/jDev_keroro_java_practic_numberstring/Nuber.java
+++ src/org/jDev_keroro_java_practic_numberstring/Nuber.java
@@ -7,6 +7,8 @@ public class Nuber {
 		System.out.println("생활코딩");
 		System.out.println("생활코딩2");
 		System.out.println("생");	//큰따옴표로 묶여 있기 때문에 문자가 아니라 문자열이된다.
+		System.out.println("생활코딩"+"입니다"); // 문자열 더하기 문자열	
+		System.out.println("생활코딩 \"큰 따옴표를 표시하고 싶다!\""); // \기호를 이용하여 큰따옴표를 만들수 있다.
 	}
 
 }
