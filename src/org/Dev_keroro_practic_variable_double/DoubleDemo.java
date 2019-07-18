8955ef68335097172b1f0a94b822d8074a64edc2
angelred551 <angelred551@gmail.com>
2019-07-19 00:23:40 +0900

    변수2
    정수와 실수
    Java는 변수의 선언과 할당이 엄격하다
    Int는 정수형, Double은 실수형으로 강제한다.

---
+++ src/org/Dev_keroro_practic_variable_double/DoubleDemo.java
@@ -0,0 +1,12 @@
+package org.Dev_keroro_practic_variable_double;
+
+public class DoubleDemo {
+
+	public static void main(String[] args) {
+		double a;
+		a = 1.1;
+		System.out.println(a+1.1);
+
+	}
+
+}
