cada8bdda983d90e6fadff2908b197e1dbe503a4
angelred551 <angelred551@gmail.com>
2019-07-18 18:41:54 +0900

    숫자와 문자2
    1. 큰따옴표, 작은따옴표를 이용한 문자와 문자열의 구분

--- src/org/jDev_keroro_java_practic_numberstring/Nuber.java
+++ src/org/jDev_keroro_java_practic_numberstring/Nuber.java
@@ -5,6 +5,8 @@ public class Nuber {
 	public static void main(String[] args) {
 		System.out.println(1+2);
 		System.out.println("생활코딩");
+		System.out.println("생활코딩2");
+		System.out.println("생");	//큰따옴표로 묶여 있기 때문에 문자가 아니라 문자열이된다.
 	}
 
 }
