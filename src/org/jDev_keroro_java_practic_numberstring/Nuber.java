1a6d681fb6c67189fb28fda37e0ce2c5fb228d82
angelred551 <angelred551@gmail.com>
2019-07-18 18:38:22 +0900

    숫자와 문자
    1. 숫자 계산방법
    2. 문자, 문자열의 사용방법
      - 문자(한글자)일 경우 ' ' 작은 따옴표를 이용하여 사용한다.
      - 문자열 (한글자 이상)일 경우  " " 큰 따옴표를 이용하여 사용한다. (작은 따옴표 사용시 에러발생.)

---
+++ src/org/jDev_keroro_java_practic_numberstring/Nuber.java
@@ -0,0 +1,10 @@
+package org.jDev_keroro_java_practic_numberstring;
+
+public class Nuber {
+
+	public static void main(String[] args) {
+		System.out.println(1+2);
+		System.out.println("생활코딩");
+	}
+
+}
