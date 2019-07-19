414fedaef50723c9aa176fe703f860ff9748081b
angelred551 <angelred551@gmail.com>
2019-07-19 10:30:33 +0900

    데이터 타입
    1. int의 표현범위는 -127~128로 1byte사이즈 이지만 CPU처리속도와 메모리 절약에 장점이 있다.
    2. 실수는 float, double 이 있다 .
    3. 문자 char 는 2byte를 사용하며 모든 유니코드 문자를 표현할 수 있다.
    
    -> 결과적으로 말해서 정수를 저장할 때는 int를 사용하면 된다

---
+++ src/org/Dev_keroro/java/datatype/IntDatatypeDemo.java
@@ -0,0 +1,14 @@
+package org.Dev_keroro.java.datatype;
+
+public class IntDatatypeDemo {
+
+	public static void main(String[] args) {
+		//byte a;
+		long b;
+		/* a = 128;	//byte는 128을 표현할수 없어 에러 발생한다. byte -128~127 
+		 byte를 사용하는 장점은 CPU처리속도가 빠르고 메모리 절약에 강점이 있다. */
+		b = 128;
+
+	}
+
+}
