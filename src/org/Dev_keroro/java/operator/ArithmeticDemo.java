864f3b75a52872726737c1e6d24f57d1315faeb7
angelred551 <angelred551@gmail.com>
2019-07-19 19:21:01 +0900

    연산자
    1. 산술연산자, C언어랑 다를게 없다.
    2. +연산자 숫자와 숫자 뿐 아니라 문자열과 문자열의 더하기도 가능하다.

---
+++ src/org/Dev_keroro/java/operator/ArithmeticDemo.java
@@ -0,0 +1,24 @@
+package org.Dev_keroro.java.operator;
+
+public class ArithmeticDemo {
+
+	public static void main(String[] args) {
+		int result = 1+2;
+		System.out.println(result);
+		
+		result = result -1;
+		
+		result = result *2;
+		System.out.println(result);
+		
+		result = result / 2;
+		System.out.println(result);
+		
+		result = result +8;
+		
+		result = result % 7;
+		System.out.println(result);
+
+	}
+
+}
