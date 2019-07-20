96627327cd3e893b58d0ec6fd2cd54d927ac8363
angelred551 <angelred551@gmail.com>
2019-07-20 10:34:23 +0900

    연산자 2
    1. 나누기. 정수 나누기 정수의 소수점은 데이터 표현범위를 넘기 때문에 버린다.
    2. 정수 나누기 실수 (실수 나누기 정수) 일 경우는 정수데이터를 데이터 표현범위가 넓은 실수로 암시적 형변환을 자동으로 해준 뒤 계산되어 실수계산된다.

---
+++ src/org/Dev_keroro/java/operator/OperatorDemo.java
@@ -0,0 +1,18 @@
+package org.Dev_keroro.java.operator;
+
+public class OperatorDemo {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 3;
+		
+		float c = 10.0f;
+		float d = 3.0f;
+		
+		System.out.println(a/b);	// 3
+		System.out.println(c/d);	// 3.333333
+		System.out.println(a/d);	// 3.333333
+
+	}
+
+}
