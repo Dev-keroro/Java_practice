f141512eac79c2403b5359b08e52cb1cad490d5a
angelred551 <angelred551@gmail.com>
2019-07-23 19:48:34 +0900

    조건무
    if, if-else, if-else if-else, switch문 C언어와 동일하다.

---
+++ src/org/Dev_keroro/java/condition/LogicDemo.java
@@ -0,0 +1,17 @@
+package org.Dev_keroro.java.condition;
+
+public class LogicDemo {
+
+	public static void main(String[] args) {
+		String id = args[0];
+		
+		if(id.equals("keroro")) {
+			System.out.println("right");
+		}else {
+			System.out.println("left");
+		}
+			
+
+	}
+
+}
