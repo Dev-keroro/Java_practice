6f0a48af283eb62a4647290271dc698c2f03cecb
angelred551 <angelred551@gmail.com>
2019-07-26 10:12:46 +0900

    클래스와 인스턴스 그리고 객체
    1. 매소드와 객체의 차이
     - 매소드는 짜여진 틀에 맞춰서 사용할 수밖에 없다.
     - 사용자가 이미 짜여진 틀(기능)중에 필요한 부분만 유동적으로 사용할 수 있다. (재사용성이 유연하다.)
    
    2. 클래스
     - 기능과 연관되어 있는 변수와 메소드의 집합 (설계도)
    
    3. 인스턴스
     - 사용자가 실제로 객체를 이용하기 위해 만드는 제품 (new xxxx)
    
    -> 클래스가 인스턴스화 되었을 때 객체라 정의한다.

---
+++ src/org/Dev_keroro/java/object/CalulatorDemo.java
@@ -0,0 +1,10 @@
+package org.Dev_keroro.java.object;
+
+public class CalulatorDemo {
+
+	public static void main(String[] args) {
+		System.out.println(10 + 40);
+		System.out.println(20 + 40);
+	}
+
+}
